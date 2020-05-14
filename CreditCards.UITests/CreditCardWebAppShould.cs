9d2737741ee58192881aac4db7f883395f79b50c
taras.shevchenko <taras.shevchenko@overnightqa.com>
2020-05-14 15:22:29 +0300

    homeButtonWithLinqByText

--- CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -151,6 +152,8 @@ namespace CreditCards.UITests
                 //    driver.FindElement(By.Name("search"));
                 //searchFieldElement.SendKeys("Luxoft");
 
+                IWebElement homeButtonWithLinqByText = driver.FindElements(By.CssSelector(".b-head ul > li"))
+                    .FirstOrDefault(x => x.Text == "ГЛАВНАЯ");
 
                 //TODO By.TagName("li") - really bad selector, try something more specific 
                 ReadOnlyCollection<IWebElement> searchButtonElements =
